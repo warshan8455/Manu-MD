e493fe0fe3210da8adf367a9dd590053b95d89f8
warshan8455 <hiruniwarthana8@gmail.com>
2025-03-07 11:42:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&JTEwjSoA#POqBMjOBa7qlrKH5JvRkyQW7w0fWupWLhRTFrZp2V9k",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || "put your github usearname correctly",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "put your github auth token",
 };
